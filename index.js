437205866044ff1d3b026038cdf9e71c7aad9a7b
Fábio Abreu <fabio.h.abreu99@gmail.com>
2025-02-11 09:18:49 -0300

    updating the post method

--- index.js
+++ index.js
@@ -12,9 +12,22 @@ app.get('/', (req, res) => {
 
 // Rota POST para receber dados
 app.post('/', (req, res) => {
-    // Log do corpo da requisição
-    console.log('Corpo da requisição recebido:', req.body);
-    console.log('Another info:', req.body.message.messageContextInfo);
+    // Log do corpo da requisição completo
+    console.log('Corpo da requisição recebido:', JSON.stringify(req.body, null, 2));
+
+    // Verifica se req.body.body existe e contém a propriedade message
+    if (req.body.body && req.body.body.message) {
+        console.log('Conteúdo de message:', req.body.body.message);
+
+        // Verifica se messageContextInfo existe dentro de message
+        if (req.body.body.message.messageContextInfo) {
+            console.log('messageContextInfo:', req.body.body.message.messageContextInfo);
+        } else {
+            console.log('messageContextInfo não está presente em message.');
+        }
+    } else {
+        console.log('message não está presente no corpo da requisição.');
+    }
 
     // Resposta ao cliente
     res.send('Dados recebidos com sucesso!');
