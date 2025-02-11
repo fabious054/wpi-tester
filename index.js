266e3a5566a9c0b417406827cc93d18755e28fc6
Fábio Abreu <fabio.abreu@robozz.com.br>
2025-02-11 08:58:02 -0300

    new updates

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+
+// Middleware para processar corpos de requisições no formato JSON ou URL-encoded
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Rota POST para receber dados
+app.post('/', (req, res) => {
+    // Log do corpo da requisição
+    console.log('Corpo da requisição recebido:', req.body);
+
+    // Resposta ao cliente
+    res.send('Dados recebidos com sucesso!');
+});
+
+// Inicia o servidor na porta 3000
+app.listen(3000, () => {
+    console.log('Servidor rodando na porta 3000');
+});
